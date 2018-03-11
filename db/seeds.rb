8680750514bfaef3dbe9a966d3da24d90243e858
alexv7 <alexvarjabedian7@gmail.com>
2018-03-11 19:09:16 -0400

    scaffold products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do |i|
+  Product.create(title: Faker::Commerce.product_name, description: Faker::Lorem.paragraph, image_url: Faker::LoremPixel.image, price: rand(1000))
+end
