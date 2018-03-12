5251f6548a589d705beb55abb9b8f3e38fea6e55
alexv7 <alexvarjabedian7@gmail.com>
2018-03-12 00:43:40 -0400

    add line item to cart increases its quantity

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,13 @@
 class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
+
+  def add_product(product_id)
+    current_item = self.line_items.find_by(product_id: product_id) # find_by returns one vs where returns an array
+    if current_item
+      current_item.quantity += 1
+    else
+      current_item = self.line_items.build(product_id: product_id)
+    end
+    current_item
+  end
 end
