ea01c6f7424a5015e80e7a9b0b449f58be4eaa8d
alexv7 <alexvarjabedian7@gmail.com>
2018-03-13 13:55:12 -0400

    users and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+
+  controller :sessions do
+    get 'login' => :new          # match '/login' => 'sessions#new', via: ['get'], :as => 'login'
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+
+
   resources :users
   resources :orders
   resources :line_items
   resources :carts
   resources :products do 
-    get :who_bought, on: :member
+    get :who_bought, on: :member # '/products/:id/who_bought'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
